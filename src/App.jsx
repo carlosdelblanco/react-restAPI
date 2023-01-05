b1744ae545bb4451b45eac16e67957e5ecb1ef46
Carlos del Blanco <delblanco.carlos@gmail.com>
2023-01-06 00:38:40 +0100

    Add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import React, { useEffect, useState } from "react";
+import CharacterList from "./components/characterList";
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch("https://rickandmortyapi.com/api/character");
-      const data = await response.json();
-      setCharacters(data.results);
-    }
-    fetchData();
-  }, []);
-
   return (
     <div>
       <h1>App Rick and Morty</h1>
-      {characters.map((character) => {
-        return (
-          <div>
-            <h2> {character.name}</h2>
-            <img src={character.image} alt={character.name} />
-          </div>
-        );
-      })}
+
+      <CharacterList />
     </div>
   );
 }
