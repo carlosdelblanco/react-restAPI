4c13b03a55654309db1fef7e51e903e08f912029
Carlos del Blanco <delblanco.carlos@gmail.com>
2023-01-07 00:47:21 +0100

    add loading messaage

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -3,22 +3,38 @@ import Character from "./Character";
 
 function CharacterList() {
   const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       const response = await fetch("https://rickandmortyapi.com/api/character");
       const data = await response.json();
+      setLoading(false);
       setCharacters(data.results);
     }
 
     fetchData();
   }, []);
 
+  if (loading) {
+    return <div>Loading</div>;
+  }
+
   return (
-    <div>
-      {characters.map((character) => {
-        return <Character key={character.id} character={character} />;
-      })}
+    <div className="container">
+      {loading ? (
+        <h1>Loading ...</h1>
+      ) : (
+        <div className="row">
+          {characters.map((character) => {
+            return (
+              <div className="col-md-4" key={character.id}>
+                <Character character={character} />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
